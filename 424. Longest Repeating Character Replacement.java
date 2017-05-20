eeb881074fd575d06c5b5d55c40a6c67e7d50d1a
Wu, Hang <wuhang0613@gmail.com>
2017-05-20 16:13:53 -0400

    Update 424. Longest Repeating Character Replacement.java

--- 424. Longest Repeating Character Replacement.java
+++ 424. Longest Repeating Character Replacement.java
@@ -1,5 +1,17 @@
 public class Solution {
     public int characterReplacement(String s, int k) {
-      
+        int len = s.length();
+        // two pointers
+        int head = 0;
+        int maxSize = 0;
+        int[] map = new int[256];
+        for (int i = 0; i < len; i++) {
+            max = Math.max(max, ++map[s.charAt(i)]);
+            while (i - head + 1 > max + k) {
+                --map[s.charAt(head++)];
+            }
+            maxSize = Math.max(maxSize, i - head + 1);
+        }
+        return maxSize;
     }
 }
