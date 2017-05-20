a75d77d4bc6d9476a11ac3aef2d70c4b23ab9868
Wu, Hang <wuhang0613@gmail.com>
2017-05-20 19:04:07 -0400

    Update 84. Largest Rectangle in Histogram.java

--- 84. Largest Rectangle in Histogram.java
+++ 84. Largest Rectangle in Histogram.java
@@ -1,3 +1,22 @@
 public class Solution {
-    public int largestRectangleArea(int[] heights) {}
+    public int largestRectangleArea(int[] heights) {
+        // Similiar idea with trapping water
+        Stack<Integer> s = new Stack()<>();
+        int max = 0;
+        int len = heights.length;
+        
+        for (int i = 0; i < len; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                int tmp = s.pop();
+                int left = s.isEmpty() ? 0 : s.peek() + 1;
+                max = Math.max(max, heights[tmp] * (i - left));
+            }
+        }
+        while (!s.isEmpty() && heights[s.peek()] >= 0) {
+            int tmp = s.pop();
+            int left = s.isEmpty() ? 0 : s.peek() + 1;
+            max = Math.max(max, heights[tmp] * (len - left));
+        }
+        return max;
     }
+}
