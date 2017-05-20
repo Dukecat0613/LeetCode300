65f63288bd00ffaffe8a33369779f4aa819c22c6
Wu, Hang <wuhang0613@gmail.com>
2017-05-20 17:03:07 -0400

    Update 65. Valid Number.java

--- 65. Valid Number.java
+++ 65. Valid Number.java
@@ -1,1 +1,35 @@
-
+public class Solution {
+    public boolean isNumber(String s) {
+      //state machine
+      boolean numseen = false;
+      boolean allowE = true;
+      boolean point = false;
+      boolean Eseen = false;
+      s=s.trim();
+      for (int i = 0; i < s.length(); i++) {
+          char tmp = s.charAt(i);
+          if (tmp == '.') {
+              if (point || Eseen) {
+                return false;
+              }
+              point = true;
+          } else if (tmp == 'e') {
+            if (Eseen || !numseen) {
+              return false;
+            }
+            Eseen = true;
+            allowE = false;
+          } else if (tmp >= '0' && tmp <= '9') {
+            numseen = true;
+            allowE = true;
+          } else if (tmp == '+' || tmp == '-') {
+            if (i > 0 && s.charAt(i - 1) != 'e') {
+              return false;
+            }
+          } else {
+            return false;
+          }
+      }
+      return allowE && numseen;
+    }
+}
