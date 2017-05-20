7596481abc8ecb2112fb736015b52f5d617ef26f
Wu, Hang <wuhang0613@gmail.com>
2017-05-20 17:49:23 -0400

    Update 11. Container With Most Water.java

--- 11. Container With Most Water.java
+++ 11. Container With Most Water.java
@@ -1,1 +1,18 @@
-
+public class Solution {
+    public int maxArea(int[] height) {
+        // two pointers
+        int len = height.length;
+        int head = 0;
+        int tail = len - 1;
+        int max = 0;
+        while (head < tail) {
+            max = Math.max(max, Math.min(height[head], height[tail]) * (tail - head));
+            if (height[head] < height[tail]) {
+                head++;
+            } else {
+                tail--;
+            }
+        }
+        return max;
+    }
+}
