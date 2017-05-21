ac368ef18ba8fbafb395df632ac889450203d7d7
Wu, Hang <wuhang0613@gmail.com>
2017-05-21 12:17:22 -0400

    Create 273. Integer to English Words.java

---
+++ 273. Integer to English Words.java
@@ -0,0 +1,6 @@
+public class Solution {
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+    public String numberToWords(int num) {
