ba0c8ee121922e170cecec3d4214aa59afd156c3
Wu, Hang <wuhang0613@gmail.com>
2017-05-21 10:29:02 -0400

    Update 99. Recover Binary Search Tree.java

--- 99. Recover Binary Search Tree.java
+++ 99. Recover Binary Search Tree.java
@@ -1,2 +1,34 @@
 public class Solution {
+    TreeNode errOne = null;
+    TreeNode errTwo = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
+        // inorder traversal of BST is always sorted
+        // once the previous node is greater than later node
+        // there must be somthing wrong
+        inorder(root);
+        if (errOne != null) {
+            int tmp = errOne.val;
+            errOne.val = errTwo.val;
+            errTwo.val = tmp;
+        }
+    }
+    
+    public void inorder (TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        // sth wrong
+        if (prev != null && prev.val > root.val) {
+            if (errOne == null) {
+                errOne = prev;
+                errTwo = root;
+            } else {
+                errTwo = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+}
