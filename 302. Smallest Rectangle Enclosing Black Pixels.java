e9afe0b11dd736bfd399e2e2a9e6a708bc24d5bf
Wu, Hang <wuhang0613@gmail.com>
2017-05-21 10:16:20 -0400

    Update 302. Smallest Rectangle Enclosing Black Pixels.java

--- 302. Smallest Rectangle Enclosing Black Pixels.java
+++ 302. Smallest Rectangle Enclosing Black Pixels.java
@@ -1,42 +1,42 @@
 public class Solution {
     public int minArea(char[][] image, int x, int y) {
         // Simple BFS, nothing to explain
-        Queue<int[]> queue=new LinkedList<>();
-        int m=image.length;
-        if(m==0) return 0;
-        int n=image[0].length;
-        int left=x;
-        int right=x;
-        int top=y;
-        int bottom=y;
-        boolean[][] visited=new boolean[m][n];
-        queue.offer(new int[]{x,y});
-        visited[x][y]=true;
-        while (!queue.isEmpty()){
-            int [] temp=queue.poll();
-            int i=temp[0];
-            int j=temp[1];
-            if(i<left) left=i;
-            if(i>right) right=i;
-            if(j<bottom) bottom=j;
-            if(j>top) top=j;
-            if(i-1>=0&&image[i-1][j]=='1'&&!visited[i-1][j]){
-            	visited[i-1][j]=true;
-                queue.offer(new int[]{i-1,j});
+        Queue<int[]> queue = new LinkedList<>();
+        int m = image.length;
+        if(m == 0) return 0;
+        int n = image[0].length;
+        int left = x;
+        int right = x;
+        int top = y;
+        int bottom = y;
+        boolean[][] visited = new boolean[m][n];
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            int i = temp[0];
+            int j = temp[1];
+            if(i < left) left = i;
+            if(i > right) right = i;
+            if(j < bottom) bottom = j;
+            if(j > top) top = j;
+            if(i - 1 >= 0 && image[i - 1][j] == '1' && !visited[i-1][j]) {
+            	visited[i - 1][j] = true;
+                queue.offer(new int[]{i - 1,j});
             }
-            if(i+1<m&&image[i+1][j]=='1'&&!visited[i+1][j]){
-            	visited[i+1][j]=true;
-                queue.offer(new int[]{i+1,j});
+            if(i + 1 < m && image[i + 1][j] == '1' && !visited[i + 1][j]) {
+            	visited[i+1][j] = true;
+                queue.offer(new int[]{i + 1,j});
             }
-            if(j-1>=0&&image[i][j-1]=='1'&&!visited[i][j-1]){
-            	visited[i][j-1]=true;
-                queue.offer(new int[]{i,j-1});
+            if(j - 1 >= 0 && image[i][j - 1] == '1' && !visited[i][j - 1]) {
+            	visited[i][j - 1] = true;
+                queue.offer(new int[]{i,j - 1});
             }
-            if(j+1<n&&image[i][j+1]=='1'&&!visited[i][j+1]){
-            	visited[i][j+1]=true;
-                queue.offer(new int[]{i,j+1});
+            if(j + 1 < n && image[i][j + 1]=='1' && !visited[i][j + 1]) {
+            	visited[i][j + 1] = true;
+                queue.offer(new int[]{i,j + 1});
             }
         }
-        return (top-bottom+1)*(right-left+1);
+        return (top - bottom + 1) * (right - left + 1);
     }
 }
