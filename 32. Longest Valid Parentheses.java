bb079d6a0fc6ad78d2f577a46cc7c4efaad687b8
Wu, Hang <wuhang0613@gmail.com>
2017-05-22 18:47:34 -0400

    Update 32. Longest Valid Parentheses.java

--- 32. Longest Valid Parentheses.java
+++ 32. Longest Valid Parentheses.java
@@ -1,2 +1,28 @@
 public class Solution {
     public int longestValidParentheses(String s) {
+        // Stack
+        Stack<Integer> st = new Stack<>();
+        int left = -1;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                st.push(i);
+            } else {
+                if (st.isEmpty()) {
+                    left = i;
+                } else {
+                    st.pop();
+                    if (st.isEmpty()) {
+                        // perferct situation
+                        max = Math.max(max, i - left);
+                    } else {
+                        // if stack is not empty, we should calculate the current valid length
+                        max = Math.max(max, i - st.peek());
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
