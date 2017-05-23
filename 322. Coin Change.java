6115f5544eaa7e473e89d557c48076909c25177a
Wu, Hang <wuhang0613@gmail.com>
2017-05-23 12:16:31 -0400

    Update 322. Coin Change.java

--- 322. Coin Change.java
+++ 322. Coin Change.java
@@ -1,5 +1,20 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        
+        int[] CC = new int[amount + 1];
+        Arrays.fill(CC, -1);
+        CC[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    if (CC[i - coins[j]] < min && CC[i - coins[j]] != -1) {
+                        min = CC[i - coins[j]] + 1;
+                        CC[i] = min;
+                    }
+                }
+            }
+            
+        }
+        return CC[amount];
     }
 }
