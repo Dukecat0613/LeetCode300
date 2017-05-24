9ff1b98da46e9a64da23a49603015f31894119f1
Wu, Hang <wuhang0613@gmail.com>
2017-05-24 13:58:34 -0400

    Update 493. Reverse Pairs.java

--- 493. Reverse Pairs.java
+++ 493. Reverse Pairs.java
@@ -1,5 +1,24 @@
 public class Solution {
     public int reversePairs(int[] nums) {
+        return merge(nums, 0, nums.length - 1);
+    }
+    
+    public int merge(int[] nums, int begin, int end) {
+        if (begin >= end) {
+            return 0;
+        }
         
+        int mid = (begin + end) >>> 1;
+        int i = begin;
+        int j = mid + 1;
+        int count = merge(nums, begin, mid) + merge(nums, mid + 1, end);
+        for (; i <= mid; i++) {
+            while (j <= end && (long)nums[i] > 2* (long)nums[j]) {
+                j++;
+            }
+            count += j - (mid + 1);
+        }
+        Arrays.sort(nums, begin, end + 1);
+        return count;
     }
 }
