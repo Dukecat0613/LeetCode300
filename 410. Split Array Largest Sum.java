2d4846ce20efcce41cbfe0baaa2d69087ab7d9a0
Wu, Hang <wuhang0613@gmail.com>
2017-05-23 23:07:20 -0400

    Update 410. Split Array Largest Sum.java

--- 410. Split Array Largest Sum.java
+++ 410. Split Array Largest Sum.java
@@ -1,5 +1,25 @@
 public class Solution {
     public int splitArray(int[] nums, int m) {
-        
+        int n = nums.length;
+        int[] s = new int[n + 1];
+        s[0] = nums[0];
+        for (int i = 1; i <= n; i++) {
+            s[i] = nums[i - 1] + s[i - 1];
+        }
+        // dp[i][j] represents the min(max subarray) of spliting 0 - j by i subarraies
+        // dp[i][j] = min(dp[i - 1][k], sum(k - j)) i -1 <= k < j
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                for (int k = i - 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][k], s[j] - s[k]));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
