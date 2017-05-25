9969dba79c1d097bea07d0cf6e5a585c947795be
Wu, Hang <wuhang0613@gmail.com>
2017-05-25 16:46:54 -0400

    Update 214. Shortest Palindrome.java

--- 214. Shortest Palindrome.java
+++ 214. Shortest Palindrome.java
@@ -1,2 +1,32 @@
 public class Solution {
     public String shortestPalindrome(String s) {
+        int len = s.length();
+        String temp = s + "*" + new StringBuilder(s).reverse().toString();
+        int table = KMP(s);
+        return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
+    
+    }
+    
+    public int[] KMP(String s) {
+        int len = s.length();
+        int[] res = new int[len];
+        int i = 0;
+        int j = 0;
+        while (i < len - 1) {
+            i++;
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+                res[i] = j;
+            } else {
+                while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                    j = res[j - 1];
+                }
+                if (s.charAt(i) == s.charAt(j)) {
+                    j++;
+                    res[i] = j;
+                }
+            }
+        }
+        return res;
+    }
+}
