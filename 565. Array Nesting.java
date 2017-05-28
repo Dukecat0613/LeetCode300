66999661416db7af79cfce42400a14d7e3ad01f1
Wu, Hang <wuhang0613@gmail.com>
2017-05-27 23:20:45 -0400

    Update 565. Array Nesting.java

--- 565. Array Nesting.java
+++ 565. Array Nesting.java
@@ -1,2 +1,21 @@
 public class Solution {
     public int arrayNesting(int[] nums) {
+        int len = nums.length;
+        boolean[] visited = new boolean[len];
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            int index = i;
+            int length = 0;
+            while (!visited[index]) {
+                visited[index] = true;
+                index = nums[index];
+                length++;
+            }
+            max = Math.max(max, length);
+        }
+        return max;
+    }
+}
